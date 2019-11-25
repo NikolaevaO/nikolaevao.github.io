1582d4a15c90bf696b04544165d5d4fcad6a3d8f
NikolaevaO <oksana.bazyk@gmail.com>
2019-11-26 00:38:29 +0300

    Добавлен раздел skills

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.addEventListener ("scroll", checkButton = () => {
   let btn = document.getElementById ("goTop");
-  if (document.documentElement.scrollTop > 250) {
+  if (document.documentElement.scrollTop > 300) {
     btn.style.visibility = "visible";
   } else {
     btn.style.visibility = "hidden";
